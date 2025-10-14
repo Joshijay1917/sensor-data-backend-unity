130c6f8b46d83f4aa1ee51ea86f492fe0669d114
= <jayjoshi1912007@gmail.com>
2025-10-13 18:12:39 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import http from "http"
+import { app } from "./app.js"
+import { Server } from "socket.io"
+
+const port = 3000
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log("A new client connected with id:", socket.id);
+    
+    socket.on("sensors", (data) => {
+        console.log("Get sensors ", data);
+
+        io.emit("sendToUnity", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected with id:", socket.id);
+    })
+})
+
+server.listen(port, () => {
+    console.log("Server is running on port ", port);
+})
