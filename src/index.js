10def0504e1fdfd7d528721ffbfa81e080f1de95
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-10-31 19:02:41 +0530

    hand detect function remove to check server

--- src/index.js
+++ src/index.js
@@ -38,25 +38,25 @@ io.on('connection', (socket) => {
     })
 })
 
-async function detectHand() {
-  const img = await loadImage('./hand.jpg')
-  const canvas = createCanvas(img.width, img.height)
-  const ctx = canvas.getContext('2d')
-  ctx.drawImage(img, 0, 0)
+// async function detectHand() {
+//   const img = await loadImage('./hand.jpg')
+//   const canvas = createCanvas(img.width, img.height)
+//   const ctx = canvas.getContext('2d')
+//   ctx.drawImage(img, 0, 0)
 
-  const hands = await tf.tidy(() => detector.estimateHands(canvas))
+//   const hands = await tf.tidy(() => detector.estimateHands(canvas))
 
-  if (hands.length > 0) {
-    const points = hands[0].keypoints3D || hands[0].keypoints
-    io.emit('handData', points)
-    console.log('✋ Sent', points.length, 'points')
-  }
+//   if (hands.length > 0) {
+//     const points = hands[0].keypoints3D || hands[0].keypoints
+//     io.emit('handData', points)
+//     console.log('✋ Sent', points.length, 'points')
+//   }
 
-  tf.engine().startScope() // optional, if used inside loops
-  tf.engine().endScope()
-}
+//   tf.engine().startScope() // optional, if used inside loops
+//   tf.engine().endScope()
+// }
 
-setInterval(detectHand, 1000)
+// setInterval(detectHand, 1000)
 
 server.listen(port, () => {
     console.log("Server is running on port ", port);
