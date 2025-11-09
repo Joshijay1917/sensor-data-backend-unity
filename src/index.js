1874d72d31d607e024876bf62f08ce007654fec5
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-11-09 14:32:03 +0530

    Implement WebRTC

--- src/index.js
+++ src/index.js
@@ -15,17 +15,34 @@ const io = new Server(server, {
     }
 })
 
-let detector = null;
-async function initModel() {
-    detector = await handpose.createDetector(handpose.SupportedModels.MediaPipeHands, {
-        runtime: 'tfjs',
-    })
-    console.log('🤖 HandPose model loaded!')
-}
-await initModel()
+const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
 
 io.on('connection', (socket) => {
     console.log("A new client connected with id:", socket.id);
+    socket.on("join-room", (data) => {
+        console.log("Data=", data);
+        const { roomId, emailId } = data;
+        console.log("User ", emailId, " Joined room ", roomId);
+        emailToSocketMapping.set(emailId, socket.id);
+        socketToEmailMapping.set(socket.id, emailId);
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit("user-joined", { emailId })
+    })
+
+    socket.on("call-user", data => {
+        const { emailId, sdp, type } = data;
+        const fromEmail = socketToEmailMapping.get(socket.id);
+        const socketId = emailToSocketMapping.get(emailId);
+        console.log("Sending offer SDP is: " + sdp + " and Type is: " + type + " from this email: " + fromEmail + " To " + emailId);
+        socket.to(socketId).emit("incomming-call", { from: fromEmail, sdp, type })
+    })
+
+    socket.on("call-accepted", data => {
+        const { emailId, sdp, type } = data;
+        const socketId = emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit("call-accepted", { sdp, type })
+    })
 
     socket.on("sensors", (data) => {
         console.log("Get sensors ", data);
@@ -37,26 +54,6 @@ io.on('connection', (socket) => {
         console.log("Client disconnected with id:", socket.id);
     })
 })
-
-async function detectHand() {
-  const img = await loadImage('./hand.jpg')
-  const canvas = createCanvas(img.width, img.height)
-  const ctx = canvas.getContext('2d')
-  ctx.drawImage(img, 0, 0)
-
-  const hands = await tf.tidy(() => detector.estimateHands(canvas))
-
-  if (hands.length > 0) {
-    const points = hands[0].keypoints3D || hands[0].keypoints
-    io.emit('handData', points)
-    console.log('✋ Sent', points.length, 'points')
-  }
-
-  tf.engine().startScope() // optional, if used inside loops
-  tf.engine().endScope()
-}
-
-setInterval(detectHand, 30000)
 
 server.listen(port, () => {
     console.log("Server is running on port ", port);
